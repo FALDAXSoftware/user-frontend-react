d3a51459086ecb4caa13838a95b9e38aba1fd6a4
anki <coc.ankit.7878@gmail.com>
2018-09-05 15:00:42 +0530

    React Initial code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Welcome to Faldax </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
