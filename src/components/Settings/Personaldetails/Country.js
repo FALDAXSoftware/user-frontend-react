095a1ee0c1b935a75c217815e3e0c2cef07008a7
Meghal <meghal.patel@openxcellinc.com>
2018-10-27 08:56:39 +0530

    QA solved

--- src/components/Settings/Personaldetails/Country.js
+++ src/components/Settings/Personaldetails/Country.js
@@ -10,53 +10,52 @@ const Option = Select.Option;
 let Countries = [];
 
 
-export default class CountryPick extends React.Component
-{
+export default class CountryPick extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            countries:[],
-            country_selected:""
+        this.state = {
+            countries: [],
+            country_selected: ""
         }
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(value) {
-       /*  console.log(`selected ${value}`); */
-       this.setState({country_selected:value});
-        this.props.onCountryChange(value,"country");
+        /*  console.log(`selected ${value}`); */
+        console.log(value)
+        this.setState({ country_selected: value });
+        this.props.onCountryChange(value, "country");
     }
 
     handleBlur() {
         /* console.log('blur'); */
     }
 
-    componentDidMount() 
-    {
-        fetch(API_URL + '/users/countries',{
-        method:"get",
-        headers:{
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            Authorization:"Bearer " + this.props.isLoggedIn
-        }})
-        .then(response => response.json())
-        .then((responseData) => {
-            this.setState({countries:responseData.data,fetching:false,callOnce:true});
-            /* console.log(responseData) */
-            Countries = responseData.data;
-        
-        });
+    componentDidMount() {
+        fetch(API_URL + '/users/countries', {
+            method: "get",
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: "Bearer " + this.props.isLoggedIn
+            }
+        })
+            .then(response => response.json())
+            .then((responseData) => {
+                this.setState({ countries: responseData.data, fetching: false, callOnce: true });
+                /* console.log(responseData) */
+                Countries = responseData.data;
+
+            });
     }
-    
-    render()
-    {
+
+    render() {
         /* console.log(this.props,this.props.profileDetails.country) */
-        return(
+        return (
             <Select
                 showSearch
-                value={this.state.country_selected!==""?this.state.country_selected:this.props.profileDetails.country}
+                value={this.state.country_selected !== "" ? this.state.country_selected : this.props.profileDetails.country}
                 placeholder="Select a Country"
                 className="Country_Select"
                 dropdownClassName="country_select_drop"
@@ -65,9 +64,9 @@ export default class CountryPick extends React.Component
                 onBlur={this.handleBlur}
                 filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
             >
-                {this.state.countries.map((country,index) => <Option key={index} value={country.name}>{country.name}</Option>)}
+                {this.state.countries.map((country, index) => <Option key={index} value={country.name}>{country.name}</Option>)}
             </Select>
         );
     }
 }
-  
+
