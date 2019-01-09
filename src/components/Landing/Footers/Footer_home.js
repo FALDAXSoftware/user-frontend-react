b976f8c8e4672c89369d990b0fb5cfe1798af97f
Krina Soni <krina.soni@openxcellinc.com>
2019-01-09 09:07:51 +0530

    footer in website code enabled

--- src/components/Landing/Footers/Footer_home.js
+++ src/components/Landing/Footers/Footer_home.js
@@ -255,7 +255,7 @@ class Footer_home extends Component {
                 <Container>
                     <FooterContainer>
                         <Row>
-                            {/* <FooterLinkCol xs={24} sm={8} md={8} lg={5} xl={5}>
+                            <FooterLinkCol xs={24} sm={8} md={8} lg={5} xl={5}>
                                 <Footer_ul>
                                     <Footer_headers>Information</Footer_headers>
                                     <li style={{ cursor: "pointer" }} >
@@ -282,14 +282,14 @@ class Footer_home extends Component {
                                         Open a Ticket
                                     </li>
                                     <li style={{ cursor: "pointer" }} onClick={this.showComing}>
-                                        <Footer_Link to="/faq">
+                                        {/* <Footer_Link to="/faq"> */}
                                         FAQ
-                                        </Footer_Link>
+                                        {/* </Footer_Link> */}
                                     </li>
                                     <li style={{ cursor: "pointer" }} onClick={this.showComing}>
                                         API Documentation
                                                     </li>
-                                    <li style={{cursor:"pointer"}} onClick={this.showComing}>Language</li>
+                                    {/* <li style={{cursor:"pointer"}} onClick={this.showComing}>Language</li> */}
                                     <li style={{ cursor: "pointer" }}>
                                         <Footer_Link to="/addcoin">List Your Token</Footer_Link>
                                     </li>
@@ -302,7 +302,7 @@ class Footer_home extends Component {
                             <FooterLinkCol xs={24} sm={8} md={8} lg={5} xl={5}>
                                 <Footer_ul>
                                     <Footer_headers>Legal & Technical</Footer_headers>
-                                    <li style={{ cursor: "pointer" }} onClick={this.showComing}>
+                                    <li style={{ cursor: "pointer" }}>
                                         <Footer_Link to="/policy">Policies</Footer_Link>
                                     </li>
                                     <li style={{ cursor: "pointer" }} onClick={this.scrollMap.bind(this)}>
@@ -312,7 +312,7 @@ class Footer_home extends Component {
                                         Security
                                     </li>
                                 </Footer_ul>
-                            </FooterLinkCol> */}
+                            </FooterLinkCol>
 
                             <Col xs={24} lg={5} xl={5}>
                                 <Icon_ul_1_header>
