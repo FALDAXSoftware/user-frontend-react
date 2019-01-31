d951cc501c0f3c710629b28308b5855dbc36c494
Meghal <meghal.patel@Openxcellinc.com>
2019-01-31 11:41:29 +0530

    new commit

--- src/Globals.js
+++ src/Globals.js
@@ -11,11 +11,7 @@ export const globalVariables = {
 
     //API_URL: 'http://192.168.3.32:1337', //Krina Local URL
 
-<<<<<<< HEAD
-    // API_URL: 'https://dev-backend.faldax.com'
-=======
     //API_URL: 'https://dev-backend.faldax.com'
->>>>>>> 1722062caa98b3863044e1387774f235d1d82f11
 
     //API_URL: 'https://prod-backend.faldax.com',
 
