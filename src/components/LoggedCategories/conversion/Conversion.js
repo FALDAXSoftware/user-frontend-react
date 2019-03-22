ba766dc3ad0d5e12308ece96fc12b336d2b87f59
Kalpit <kalpit.akhawat@openxcell.info>
2019-03-22 14:12:06 +0530

    conversion screen

--- src/components/LoggedCategories/conversion/Conversion.js
+++ src/components/LoggedCategories/conversion/Conversion.js
@@ -1,21 +1,229 @@
 import React from "react";
-import { ConversionWarp, ConversionContainer, MainRow, ConversionTab, LeftCol } from "../../../styled-components/conversion/style";
+import { ConversionWarp, ConversionContainer, MainRow, ConversionTab, LeftCol, ConversionTitle, CustomRadioContainer, ConversionTabPane, ConversionRadioRow, BorderRow, RowTitle, ConversionInput, ConversionDropDown, DropDownOption, DropIcon, ConversionSubmitBtn, RightCol, RightColContainer, RightColTitle, RightColAmount, RightColPrice, DashedSeprator, LeftSpan, RightSpan, RightTotal, LeftTotal, PayWith, BankAcountDropdown } from "../../../styled-components/conversion/style";
 import Navigation from "../../Navigations/Navigation";
-import { Row, Col, Tabs } from "antd";
-const TabPane = Tabs.TabPane;
+import { Row, Col, Tabs, Select, Button, Divider, Icon } from "antd";
+const Option = Select.Option
+
 class Conversion extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 1
+        }
+        this.handleTabChange = this.handleTabChange.bind(this);
+    }
+    handleTabChange(e) {
+        console.log(e);
+        this.setState({
+            selectedTab: e
+        })
+    }
     render() {
         return (
             <ConversionWarp>
                 <Navigation></Navigation>
                 <ConversionContainer>
                     <MainRow>
-                        <LeftCol md={12}>
-                            <ConversionTab defaultActiveKey="1">
-                                <TabPane tab="BUY" key="1">Content of Tab Pane 1</TabPane>
-                                <TabPane tab="SELL" key="2">Content of Tab Pane 2</TabPane>
+                        <LeftCol lg={12}>
+                            <ConversionTab defaultActiveKey="1" onChange={this.handleTabChange}>
+                                <ConversionTabPane tab="BUY" key="1">
+                                    <Row>
+                                        <Col>
+                                            <ConversionTitle>Choose Which Assets to Trade</ConversionTitle>
+                                        </Col>
+                                    </Row>
+                                    <ConversionRadioRow>
+                                        <Col md={12}>
+                                            <CustomRadioContainer>
+                                                <input type="radio" name="fees" checked />
+                                                <span className="radio-label">Including Fees</span>
+                                                <span className="checkmark"></span>
+                                            </CustomRadioContainer>
+                                        </Col>
+                                        <Col md={12}>
+                                            <CustomRadioContainer>
+                                                <input type="radio" name="fees" />
+                                                <span className="radio-label">Excluding Fees</span>
+                                                <span className="checkmark"></span>
+                                            </CustomRadioContainer>
+                                        </Col>
+                                    </ConversionRadioRow>
+                                    <BorderRow>
+                                        <RowTitle>
+                                            You Get
+                                        </RowTitle>
+                                        <Col xs={16}>
+                                            <ConversionInput type="text" />
+                                        </Col>
+                                        <Col xs={8} style={{ height: "42px" }}>
+                                            <ConversionDropDown defaultValue="XRP">
+                                                <DropDownOption value="BTC"> <DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  BTC</DropDownOption>
+                                                <DropDownOption value="XRP"><DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  XRP</DropDownOption>
+                                                <DropDownOption value="LTC"><DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  Disabled</DropDownOption>
+                                                <DropDownOption value="ETH"><DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  ETH</DropDownOption>
+                                            </ConversionDropDown>
+                                        </Col>
+                                    </BorderRow>
+                                    <BorderRow>
+                                        <RowTitle>
+                                            You Pay
+                                        </RowTitle>
+                                        <Col xs={16}>
+                                            <ConversionInput type="text" />
+                                        </Col>
+                                        <Col xs={8} style={{ height: "42px" }}>
+                                            <ConversionDropDown defaultValue="USD" disabled>
+                                                <DropDownOption value="USD"> <DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  USD</DropDownOption>
+                                                <DropDownOption value="XRP"><DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  XRP</DropDownOption>
+                                                <DropDownOption value="LTC"><DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  Disabled</DropDownOption>
+                                                <DropDownOption value="ETH"><DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  ETH</DropDownOption>
+                                            </ConversionDropDown>
+                                        </Col>
+                                    </BorderRow>
+                                    <Row style={{ marginBottom: "25px" }}>
+                                        <Col xs={12}>
+                                            <PayWith>
+                                                Pay With
+                                            </PayWith>
+                                        </Col>
+                                        <Col xs={12} style={{ textAlign: "right" }}>
+                                            <BankAcountDropdown defaultValue="1" >
+                                                <Option value="1">Suntest ****789</Option>
+                                                <Option value="2">Silvergate ****123</Option>
+                                            </BankAcountDropdown>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <ConversionSubmitBtn type="primary" size="large" block>Buy xrp</ConversionSubmitBtn>
+                                        </Col>
+                                    </Row>
+                                </ConversionTabPane>
+                                <ConversionTabPane tab="SELL" key="2">
+                                    <Row>
+                                        <Col>
+                                            <ConversionTitle>Choose Which Assets to Trade</ConversionTitle>
+                                        </Col>
+                                    </Row>
+                                    <ConversionRadioRow>
+                                        <Col md={12}>
+                                            <CustomRadioContainer>
+                                                <input type="radio" name="fees" checked />
+                                                <span className="radio-label">Including Fees</span>
+                                                <span className="checkmark"></span>
+                                            </CustomRadioContainer>
+                                        </Col>
+                                        <Col md={12}>
+                                            <CustomRadioContainer>
+                                                <input type="radio" name="fees" />
+                                                <span className="radio-label">Excluding Fees</span>
+                                                <span className="checkmark"></span>
+                                            </CustomRadioContainer>
+                                        </Col>
+                                    </ConversionRadioRow>
+                                    <BorderRow>
+                                        <RowTitle>
+                                            You Pay
+                                        </RowTitle>
+                                        <Col xs={16}>
+                                            <ConversionInput type="text" />
+                                        </Col>
+                                        <Col xs={8} style={{ height: "42px" }}>
+                                            <ConversionDropDown defaultValue="XRP">
+                                                <DropDownOption value="BTC"> <DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  BTC</DropDownOption>
+                                                <DropDownOption value="XRP"><DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  XRP</DropDownOption>
+                                                <DropDownOption value="LTC"><DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  Disabled</DropDownOption>
+                                                <DropDownOption value="ETH"><DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  ETH</DropDownOption>
+                                            </ConversionDropDown>
+                                        </Col>
+                                    </BorderRow>
+                                    <BorderRow>
+                                        <RowTitle>
+                                            You Get
+                                        </RowTitle>
+                                        <Col xs={16}>
+                                            <ConversionInput type="text" />
+                                        </Col>
+                                        <Col xs={8} style={{ height: "42px" }}>
+                                            <ConversionDropDown defaultValue="USD" disabled>
+                                                <DropDownOption value="USD"> <DropIcon src="https://s3.us-east-2.amazonaws.com/production-static-asset/coin/defualt_coin.png" height="20px" />  USD</DropDownOption>
+                                            </ConversionDropDown>
+                                        </Col>
+                                    </BorderRow>
+                                    {/* <Row style={{ marginBottom: "25px" }}>
+                                        <Col xs={12}>
+                                            <PayWith>
+                                                Pay With
+                                            </PayWith>
+                                        </Col>
+                                        <Col xs={12} style={{ textAlign: "right" }}>
+                                            <BankAcountDropdown defaultValue="1" >
+                                                <Option value="1">Suntest ****789</Option>
+                                                <Option value="2">Silvergate ****123</Option>
+                                            </BankAcountDropdown>
+                                        </Col>
+                                    </Row> */}
+                                    <Row>
+                                        <Col>
+                                            <ConversionSubmitBtn type="primary" size="large" block style={{ marginTop: "57px" }}>SELL xrp</ConversionSubmitBtn>
+                                        </Col>
+                                    </Row>
+                                </ConversionTabPane>
                             </ConversionTab>
                         </LeftCol>
+                        <RightCol lg={12}>
+                            <RightColContainer>
+                                <Row>
+                                    <Col>
+                                        {this.state.selectedTab == 1 &&
+                                            <RightColTitle>You Are Buying</RightColTitle>
+                                        }
+                                        {this.state.selectedTab == 2 &&
+                                            <RightColTitle>You Are Selling</RightColTitle>
+                                        }
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <RightColAmount>0.0123 BTC</RightColAmount>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <RightColPrice>@ $3,914.06  per BTC</RightColPrice>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <DashedSeprator></DashedSeprator>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={12}>
+                                        <RightSpan>0.0123 BTC</RightSpan>
+                                    </Col>
+                                    <Col xs={12} style={{ textAlign: "right" }}>
+                                        <LeftSpan>$3,000</LeftSpan>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={12}>
+                                        <RightSpan>FALDAX Fee</RightSpan>
+                                    </Col>
+                                    <Col xs={12} style={{ textAlign: "right" }}>
+                                        <LeftSpan>$5.00</LeftSpan>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={12}>
+                                        <RightTotal>Total</RightTotal>
+                                    </Col>
+                                    <Col xs={12} style={{ textAlign: "right" }}>
+                                        <LeftTotal>$3005</LeftTotal>
+                                    </Col>
+                                </Row>
+                            </RightColContainer>
+                        </RightCol>
                     </MainRow>
                 </ConversionContainer>
             </ConversionWarp>
--- src/styled-components/conversion/style.js
+++ src/styled-components/conversion/style.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import { Container } from "../homepage/style";
 import { ConversionBackground } from "../../Constants/images";
-import { Row, Col, Tabs } from "antd";
+import { Row, Col, Tabs, Select, Button } from "antd";
+const TabPane = Tabs.TabPane;
+const Option = Select.Option
 export const ConversionWarp = styled.div`
     background-color:#f5f6fa;
     background-image:url('${ConversionBackground}');
@@ -20,7 +22,21 @@ export const MainRow = styled(Row)`
 `
 export const LeftCol = styled(Col)`
     background-color:#fff;
-    box-shadow: 3px 0px 5px 0px rgb( 109, 109, 109 );
+    box-shadow: 3px 0px 5px 0px rgba(109, 109, 109, 0.19);
+    position:relative;
+    &:after {
+        content: '';
+        position: absolute;
+        right: 0;
+        top: 50%;
+        width: 0;
+        height: 0;
+        border: 15px solid transparent;
+        border-left-color: #ffffff;
+        border-right: 0;
+        margin-top: -15px;
+        margin-right: -15px;
+    }
 `
 export const ConversionTab = styled(Tabs)`
     border-bottom:none;
@@ -35,6 +51,7 @@ export const ConversionTab = styled(Tabs)`
         text-align:center;
         font-size:18px;
         font-weight:700;
+        border-bottom:1px solid #e3ebfc;
     }
     &.ant-tabs-bar>.ant-tabs-nav-container>.ant-tabs-nav-wrap>.ant-tabs-nav-scroll>.ant-tabs-nav>div>.ant-tabs-tab.ant-tabs-tab-active{
         background-color:#e3ebfc;
@@ -56,5 +73,258 @@ export const ConversionTab = styled(Tabs)`
     }
     &.ant-tabs-bar>.ant-tabs-nav-container>.ant-tabs-nav-wrap>.ant-tabs-nav-scroll>.ant-tabs-nav>.ant-tabs-ink-bar{
         display:none !important;
+    }
+`
+export const ConversionTabPane = styled(TabPane)`
+    padding:25px;
+`
+export const ConversionTitle = styled.span`
+    font-size:18px;
+    font-family: "Open Sans";
+    font-weight:bold;
+    color:black;
+    padding:0px 20px 20px;
+    display:block;
+`
+export const ConversionRadioRow = styled(Row)`
+    padding:20px;
+`
+export const CustomRadioContainer = styled.label`
+    display: block;
+    position: relative;
+    padding-left: 35px;
+    margin-bottom: 12px;
+    cursor: pointer;
+    font-size: 13px;
+    font-weight:600;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+
+    & input {
+        position: absolute;
+        opacity: 0;
+        cursor: pointer;
+        height: 0;
+        width: 0;
+      }
+      & .checkmark {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 20px;
+        width: 20px;
+        border:1px solid #b1b1bf;
+        border-radius: 50%;
+      }
+      & ::hover input ~ .checkmark {
+        background-color: #fff;
+      }
+      & input:checked ~ span.radio-label{
+          color:#4c84ff;
+      }
+      & input:checked ~ .checkmark {
+        border-color:#4c84ff;
+      }
+      & .checkmark:after {
+        content: "";
+        position: absolute;
+        display: none;
+      }
+
+      & input:checked ~ .checkmark:after {
+        display: block;
+      }
+      
+      & .checkmark:after {
+        top: 4px;
+        left: 4px;
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        background: #4c84ff;
+      }
+`
+export const BorderRow = styled(Row)`
+      border:1px solid #dfe4f7;
+      border-radius:5px;
+      padding:20px;
+      margin-bottom:35px;
+`
+export const RowTitle = styled.span`
+    position: absolute;
+    top: -11px;
+    left: 20px;
+    padding: 0px 10px;
+    background-color: white;
+    font-weight: 600;
+    font-size: 13px;
+`
+export const ConversionInput = styled.input`
+    padding: 6px 15px;
+    height:auto;
+    width: 100%;
+    border: none;
+    background-color: #f6f8fe;
+    font-weight: 600;
+    font-size: 20px;
+    color:black;
+
+    &:focus{
+        border:none;
+        outline:none;
+    }
+`
+export const ConversionDropDown = styled(Select)`
+    width:100%;
+    padding:4px 0px;
+    color: black;
+    & .ant-select-selection{
+        border:none;
+        font-size:20px;
+        font-weight:600;
+        background-color: white;
+    }
+    &.ant-select-disabled .ant-select-selection{
+        cursor:text;
+    }
+    & .ant-select-selection-selected-value{
+        float:none;
+        text-align:center;
+    }
+    & .ant-select-selection:focus{
+        box-shadow: none;
+        ouline:none;
+    }
+    & .ant-select-arrow{
+        color:black;
+    }
+    &.ant-select-disabled .ant-select-arrow{
+        display:none;
+    }
+
+`
+export const DropIcon = styled.img`
+    margin-right:10px;
+`
+export const DropDownOption = styled(Option)`
+
+`
+export const ConversionSubmitBtn = styled(Button)`
+    padding:12px;
+    font-size:22px;
+    font-weight:bold;
+    text-transform:uppercase;  
+    height:auto;  
+    font-family: "Open Sans";
+    background-color: #4c84ff;
+    border-color: #4c84ff;
+
+    &:hover{
+        background-color: white;
+        border-color: #4c84ff;
+
+        color:#4c84ff;
+    }
+`
+
+export const RightCol = styled(Col)`
+    padding:40px;
+    min-height: 613px;
+    display: flex;
+    align-items: center;
+`
+export const RightColContainer = styled.div`
+    background-image: url(/images/top-zigzag-bg.png);
+    background-size: cover;
+    height:100%;
+    width:100%;
+    min-height:470px;
+    padding:40px;
+`
+
+export const RightColTitle = styled.span`
+    text-transfrom:uppercase;
+    font-weight:600;
+    font-size:14px;
+    color:#4c84ff;
+    display:block;
+    text-align:center;
+    padding-top:15px;
+`
+
+export const RightColAmount = styled.span`
+    font-weight:600;
+    font-size:50px;
+    display:block;
+    text-align:center;
+    padding-top:75px;
+    font-family: "Open Sans";
+
+`
+
+
+export const RightColPrice = styled.span`
+    font-weight:600;
+    font-size:12px;
+    display:block;
+    text-align:center;
+    color:#4c84ff;
+    padding-top:8px;
+`
+export const DashedSeprator = styled.div`
+    height:1px;
+    background-image: url(/images/dahsed-line.png);
+    background-repeat: no-repeat;
+    background-size: cover;
+    margin-top: 70px;
+    margin-bottom: 23px;
+`
+
+export const RightSpan = styled.span`
+    font-weight:600;
+    font-size:14px;
+    display:block;
+    color:rgba( 57, 65, 77, 0.702 );
+    padding:10px;
+`
+export const LeftSpan = styled.span`
+    font-weight:600;
+    font-size:14px;
+    display:block;
+    color:#39414d;
+    padding:10px;
+`
+
+export const RightTotal = styled.span`
+    font-weight:600;
+    font-size:14px;
+    display:block;
+    color:#4c84ff;
+    padding:10px;
+`
+export const LeftTotal = styled.span`
+    font-weight:600;
+    font-size:14px;
+    display:block;
+    color:#4c84ff;
+    padding:10px;
+`
+
+export const PayWith = styled.span`
+    line-height: 2;
+    font-weight: 600;
+`
+
+
+export const BankAcountDropdown = styled(Select)`
+    & .ant-select-selection{
+        border:none;
+    }    
+    & .ant-select-selection:focus{
+        box-shadow: none;
+        ouline:none;
     }
 `
