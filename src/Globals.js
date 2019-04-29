051779104b874aa4cfe8c16c09d4bcd3458bf795
Meghal <meghal.patel@openxcellinc.com>
2019-04-29 11:16:16 +0530

    changed URL

--- src/Globals.js
+++ src/Globals.js
@@ -11,13 +11,13 @@ export const globalVariables = {
 
     //API_URL: 'http://192.168.2.224:1337', //Kalpit Local URL
 
-    API_URL: 'http://192.168.1.211:1337', //Mansi's URL
+    //API_URL: 'http://192.168.1.211:1337', //Mansi's URL
 
     //API_URL: 'http://192.168.3.32:1337', //Krina Local URL
 
     //API_URL: 'http://192.168.0.93:1337'
 
-    //API_URL: 'https://dev-backend.faldax.com'
+    API_URL: 'https://dev-backend.faldax.com'
 
     //API_URL: 'https://prod-backend.faldax.com',
 
