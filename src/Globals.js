d1ec857655a40ed8a09fcace5a4249bd5aa2ebb3
Bhumi Patel <bhumi.patel@openxcellinc.com>
2019-09-24 17:09:14 +0530

    Simplex integration

--- src/Globals.js
+++ src/Globals.js
@@ -11,7 +11,7 @@ export const globalVariables = {
 
   //API_URL: 'http://18.191.87.133:8084', //Live URL
 
-  // API_URL: "http://192.168.0.213:1337" //Mansi's
+  API_URL: "http://192.168.0.213:1337" //Mansi's
 
   //API_URL: 'http://192.168.0.85:1337'
 
@@ -19,11 +19,11 @@ export const globalVariables = {
 
   //API_URL: 'http://192.168.2.224:1337', //Kalpit Local URL
 
-  //API_URL: 'http://192.168.3.32:1337', //Krina Local URL
+  // API_URL: "http://192.168.3.32:1337" //Krina Local URL
 
   //API_URL: 'http://192.168.0.93:1337'
 
-  API_URL: "https://dev-backend.faldax.com"
+  // API_URL: "https://dev-backend.faldax.com"
 
   //API_URL: 'https://prod-backend.faldax.com',
 
