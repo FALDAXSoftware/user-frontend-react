2bfa6ca76447a2ca9b4d9b88e26e01b8294ecbc2
Manthan <manthan@Manthans-Mac-mini.local>
2020-01-27 11:18:01 +0530

    Add language detector

--- src/i18n.js
+++ src/i18n.js
@@ -9,6 +9,39 @@ i18n
   .use(XHR)
   .use(LanguageDetector)
   .init({
+    detection: {
+      // order and from where user language should be detected
+      order: [
+        // "querystring",
+        "cookie",
+        "localStorage",
+        // "navigator",
+        "htmlTag"
+        // "path",
+        // "subdomain"
+      ],
+
+      // keys or params to lookup language from
+      // lookupQuerystring: "lng",
+      lookupCookie: "lan",
+      lookupLocalStorage: "lng",
+      lookupFromPathIndex: 0,
+      lookupFromSubdomainIndex: 0,
+
+      // cache user language on
+      caches: ["localStorage", "cookie"],
+      excludeCacheFor: ["cimode"], // languages to not persist (cookie, localStorage)
+
+      // optional expire and domain for set cookie
+      cookieMinutes: 10,
+      cookieDomain: "localhost",
+
+      // optional htmlTag with lang attribute, the default is:
+      htmlTag: document.documentElement,
+
+      // only detect languages that are in the whitelist
+      checkWhitelist: true
+    },
     resources: {
       en: translationEng,
       jap: translationJap
