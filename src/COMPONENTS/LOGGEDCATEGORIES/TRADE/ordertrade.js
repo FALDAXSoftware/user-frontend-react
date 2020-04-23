757b83b1f3bc66ef0068e6b661b4d61bc0fa3aa4
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-04-23 18:07:11 +0530

    my order and trade issue resolved

--- src/COMPONENTS/LOGGEDCATEGORIES/TRADE/ordertrade.js
+++ src/COMPONENTS/LOGGEDCATEGORIES/TRADE/ordertrade.js
@@ -29,7 +29,7 @@ export const OrderWrap = styled.div`
 
   &::-webkit-scrollbar-thumb {
     background-color: ${(props) =>
-      props.theme.mode === "dark" ? "#041624" : ""};
+    props.theme.mode === "dark" ? "#041624" : ""};
     border-radius: 3px;
   }
   &::-webkit-scrollbar-track {
@@ -39,7 +39,7 @@ export const OrderWrap = styled.div`
 export const HTable = styled(Table)`
   > thead {
     background-color: ${(props) =>
-      props.theme.mode === "dark" ? "#041422" : "#f5f6fa"};
+    props.theme.mode === "dark" ? "#041422" : "#f5f6fa"};
     color: #174c7e;
     border: none;
   }
@@ -54,7 +54,7 @@ export const HTable = styled(Table)`
   }
   > tbody > tr:nth-of-type(odd) {
     background-color: ${(props) =>
-      props.theme.mode === "dark" ? "#041422" : "#f9f9f9"};
+    props.theme.mode === "dark" ? "#041422" : "#f9f9f9"};
   }
 `;
 const SideType = styled.td`
@@ -106,13 +106,13 @@ class OrderTrade extends Component {
                   {self.props.pending !== 2 ? (
                     <th>{this.t("history:price_text.message")}</th>
                   ) : (
-                    <th>{this.t("limit_price_text.message")}</th>
-                  )}
+                      <th>{this.t("limit_price_text.message")}</th>
+                    )}
                   {self.props.pending !== 2 ? (
                     <th>{this.t("unfilled_text.message")}</th>
                   ) : (
-                    <th>{this.t("stop_price_text.message")}</th>
-                  )}
+                      <th>{this.t("stop_price_text.message")}</th>
+                    )}
                   <th>{this.t("fill_price_text.message")}</th>
                   <th>{this.t("type_text.message")}</th>
                   <th>{this.t("time_text.message")}</th>
@@ -120,8 +120,8 @@ class OrderTrade extends Component {
                   {self.props.pending === 2 ? (
                     <th>{this.t("actions_text.message")}</th>
                   ) : (
-                    ""
-                  )}
+                      ""
+                    )}
                 </tr>
               </thead>
             </TableHeader>
@@ -136,7 +136,7 @@ class OrderTrade extends Component {
               <TableContent cellpadding="10px" cellspacing="0" border="0">
                 <tbody>
                   {this.props.orderTradeData.length > 0 ? (
-                    this.props.orderTradeData.map(function(data) {
+                    this.props.orderTradeData.map(function (data) {
                       var date;
                       if (
                         self.props.profileDetails.date_format === "MM/DD/YYYY"
@@ -158,11 +158,46 @@ class OrderTrade extends Component {
                           .local()
                           .format("MMM D, YYYY, H:m:s");
                       var Filled = data.fix_quantity - data.quantity;
+                      // console.log("self.props.profileDetails", self.props.profileDetails)
+                      var typeValue = ""
+                      var currencyValue = ""
+                      var sideValue
+                      if (self.props.pending == 1) {
+                        if (data.user_id == self.props.profileDetails.id) {
+                          sideValue = data.side
+                        } else if (data.requested_user_id == self.props.profileDetails.id) {
+                          sideValue = (data.side == 'Buy') ? "Sell" : "Buy"
+                        }
+                        if (data.side == "Buy") {
+                          if (data.user_id == self.props.profileDetails.id) {
+                            currencyValue = data.settle_currency
+                          } else if (data.requested_user_id == self.props.profileDetails.id) {
+                            currencyValue = data.currency
+                          }
+                        } else if (data.side == "Sell") {
+                          if (data.user_id == self.props.profileDetails.id) {
+                            currencyValue = data.settle_currency
+                          } else if (data.requested_user_id == self.props.profileDetails.id) {
+                            currencyValue = data.currency
+                          }
+                        }
+                        console.log("currencyValue", currencyValue)
+                        console.log("data", data)
+                        if (data.user_id == self.props.profileDetails.id) {
+                          typeValue = data.order_type
+                        } else if (data.requested_user_id == self.props.profileDetails.id) {
+                          typeValue = "Limit"
+                        }
+                      } else {
+                        typeValue = data.order_type;
+                        currencyValue = data.settle_currency;
+                        sideValue = data.side
+                      }
                       return (
                         <tr>
-                          <SideType type={data.side}>{data.side}</SideType>
+                          <SideType type={sideValue}>{sideValue}</SideType>
                           <td>
-                            {data.quantity.toFixed(3)} {data.settle_currency}
+                            {data.quantity.toFixed(3)} {currencyValue}
                           </td>
                           {/* <td>
                             {self.props.pending !== 2
@@ -180,13 +215,13 @@ class OrderTrade extends Component {
                             {self.props.pending !== 2
                               ? Filled.toFixed(5)
                               : data.stop_price !== undefined
-                              ? data.stop_price.toFixed(5)
-                              : 0}
+                                ? data.stop_price.toFixed(5)
+                                : 0}
                           </SideType>
                           <td>
                             {data.fill_price.toFixed(5)} {data.currency}
                           </td>
-                          <td>{data.order_type}</td>
+                          <td>{typeValue}</td>
                           <td>{date}</td>
                           <td>
                             {self.props.pending === 2
@@ -211,14 +246,14 @@ class OrderTrade extends Component {
                               </span>
                             </th>
                           ) : (
-                            ""
-                          )}
+                              ""
+                            )}
                         </tr>
                       );
                     })
                   ) : (
-                    <NDF>{this.t("support:no_data_found.message")}</NDF>
-                  )}
+                      <NDF>{this.t("support:no_data_found.message")}</NDF>
+                    )}
                 </tbody>
               </TableContent>
             </Scrollbars>
