01f09e4b5c55003e2735e454c33160fbe2fdfa10
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-05-01 18:20:38 +0530

    jenkins file changes

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -98,7 +98,7 @@ module.exports = {
       // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
-      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
       new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson])
     ]
   },
@@ -324,7 +324,7 @@ module.exports = {
     // solution that requires the user to opt into importing specific locales.
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
-    // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
