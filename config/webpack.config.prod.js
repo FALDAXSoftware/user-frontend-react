98615a72adf18224b2c935d9e06bc3c135b490a7
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-05-01 17:57:25 +0530

    jenkins file changes

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -43,7 +43,7 @@ const cssFilename = "static/css/[name].[contenthash:8].css";
 // To have this structure working with relative paths, we have to use custom options.
 const extractTextPluginOptions = shouldUseRelativeAssetPaths
   ? // Making sure that the publicPath goes back to to build folder.
-    { publicPath: Array(cssFilename.split("/").length).join("../") }
+  { publicPath: Array(cssFilename.split("/").length).join("../") }
   : {};
 
 // This is the production configuration.
@@ -323,7 +323,7 @@ module.exports = {
     // solution that requires the user to opt into importing specific locales.
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
