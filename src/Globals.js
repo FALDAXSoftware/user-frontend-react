99bb0f22cb1f7a885a78d3bf16d9e0b917dd810f
Bhumi Patel <bhumi.patel@openxcellinc.com>
2020-05-12 11:25:41 +0530

    Bug fixes

--- src/Globals.js
+++ src/Globals.js
@@ -15,7 +15,7 @@ export const globalVariables = {
 
   // API_URL: "http://localhost:1440", //Mansi's
 
-  // API_URL: "http://011fa32b.ngrok.io",
+  API_URL: "http://a0821322.ngrok.io",
 
   // API_URL: "http://7b2d9029.ngrok.io",
 
