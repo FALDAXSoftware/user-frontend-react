65fd4ebcfce705e22c99f9f4637d299413ff9723
Bhumi Patel <bhumi.patel@openxcellinc.com>
2020-06-05 17:11:49 +0530

    bug fix

--- src/COMPONENTS/LOGGEDCATEGORIES/TRADE/ordertrade.js
+++ src/COMPONENTS/LOGGEDCATEGORIES/TRADE/ordertrade.js
@@ -331,7 +331,7 @@ class OrderTrade extends Component {
                               </CancelBTN>
                             </th>
                           ) : (
-                            "-"
+                            ""
                           )}
                         </tr>
                       );
