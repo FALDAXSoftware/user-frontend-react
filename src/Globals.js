8729f745799abd63c741bf79dda85de524ee3287
Bhumi Patel <bhumi.patel@openxcellinc.com>
2020-08-06 09:45:56 +0530

    Wallet restriction for tier 0 account

--- src/Globals.js
+++ src/Globals.js
@@ -7,7 +7,7 @@ export const globalVariables = {
 
   WordpressSiteURL: process.env.REACT_APP_WORDPRESS_ENDPOINT,
 
-  API_URL: "http://86c3c8f2e6e3.ngrok.io",
+  API_URL: process.env.REACT_APP_API_ENDPOINT,
 
   TRADE_URL: process.env.REACT_APP_TRADE_ENDPOINT,
 
