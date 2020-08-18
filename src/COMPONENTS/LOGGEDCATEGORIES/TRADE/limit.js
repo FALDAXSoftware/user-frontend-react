d767323d95bd1ab800a687188b7fbc4af70fd6b9
Bhumi Patel <bhumi.patel@openxcellinc.com>
2020-08-18 16:38:58 +0530

    Bug fixes

--- src/COMPONENTS/LOGGEDCATEGORIES/TRADE/limit.js
+++ src/COMPONENTS/LOGGEDCATEGORIES/TRADE/limit.js
@@ -505,18 +505,15 @@ class Limit extends Component {
         singlefiatCurrencyValue: props.userBal.currencyFiat,
       });
     }
-
-    if (props.cryptoPair !== undefined && props.cryptoPair !== "") {
-      if (props.cryptoPair.crypto !== this.state.crypto) {
-        this.setState({ crypto: props.cryptoPair.crypto }, () => {
-          this.emitAmount();
-        });
-      }
-      if (props.cryptoPair.currency !== this.state.currency) {
-        this.setState({ currency: props.cryptoPair.currency }, () => {
-          this.emitAmount();
-        });
-      }
+    if (props.crypto && props.crypto !== this.props.crypto) {
+      this.setState({ crypto: props.crypto }, () => {
+        this.emitAmount();
+      });
+    }
+    if (props.currency && props.currency !== this.props.currency) {
+      this.setState({ currency: props.currency }, () => {
+        this.emitAmount();
+      });
     }
     // if (props.sellTotal && props.sellTotal != this.props.sellTotal) {
     //   this.setState(
@@ -979,6 +976,12 @@ class Limit extends Component {
   onChangeCheck(e) {}
 
   render() {
+    console.log(
+      "test",
+      this.state.crypto,
+      this.props.crypto,
+      this.props.cryptoPair
+    );
     const {
       userBalFees,
       buyEstPrice,
