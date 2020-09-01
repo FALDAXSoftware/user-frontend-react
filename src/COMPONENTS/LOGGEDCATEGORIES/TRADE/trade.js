dabad999a3e8f0ac4f9af856190b0e9ba1568df4
Bhumi Patel <bhumi.patel@openxcellinc.com>
2020-09-01 10:51:42 +0530

    user wallet changes

--- src/COMPONENTS/LOGGEDCATEGORIES/TRADE/trade.js
+++ src/COMPONENTS/LOGGEDCATEGORIES/TRADE/trade.js
@@ -499,9 +499,11 @@ class Trade extends Component {
       });
       // this.setState({ userBalLoader: true });
       this.props.io.on("symbol-high-level-info", (data) => {
-        this.setState({
-          symbolHighLevelInfo: data,
-        });
+        if (data.name == this.state.crypto + "-" + this.state.currency) {
+          this.setState({
+            symbolHighLevelInfo: data,
+          });
+        }
       });
       this.props.io.on("users-all-trade-data", (data) => {
         this.updateMyOrder(data);
@@ -515,7 +517,22 @@ class Trade extends Component {
         this.getInstrumentData();
       }, 10000);
       this.props.io.on("user-wallet-balance", (data) => {
-        this.setState({ userBal: data, userBalLoader: false });
+        if (data == true) {
+          this.props.io.emit("user_wallet_data", {
+            user_id: this.props.profileDetails.id,
+            crypto: this.state.crypto,
+            currency: this.state.currency,
+          });
+        } else {
+          // if (data) {
+          this.setState({ userBal: data, userBalLoader: false });
+          // }
+        }
+      });
+      this.props.io.on("user-after-wallet-balance", (data) => {
+        if (data) {
+          this.setState({ userBal: data, userBalLoader: false });
+        }
       });
       this.props.io.on("spread-values", (data) => {
         if (data) {
